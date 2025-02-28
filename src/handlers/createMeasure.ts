c124242b00d00f8bae708e636de37c49fad48781
Edgard Espinoza <edgardespinoza@Edgards-MacBook-Pro.local>
2025-02-27 22:36:06 -0500

    chore: change folder build to dist

--- src/handlers/createMeasure.ts
+++ src/handlers/createMeasure.ts
@@ -18,7 +18,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEventV2): Promise<APIG
         ...response,
         headers: {
             'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': 'http://localhost:5173',
+            'Access-Control-Allow-Origin': `${process.env.DNS_CLIENT}`,
             'Access-Control-Allow-Methods': 'OPTIONS,POST',
             'Access-Control-Allow-Credentials': true,
         },
