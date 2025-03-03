dcd65f7b895ebf1e4631dde88f30470edfa48675
Edgard Espinoza <edgardespinoza@Edgards-MacBook-Pro.local>
2025-03-03 13:07:57 -0500

    fix: change allow origin

--- src/handlers/createMeasure.ts
+++ src/handlers/createMeasure.ts
@@ -18,8 +18,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEventV2): Promise<APIG
         ...response,
         headers: {
             'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': `${process.env.DNS_CLIENT}`,
-            'Access-Control-Allow-Methods': 'OPTIONS,POST',
+            'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Credentials': true,
         },
     };
